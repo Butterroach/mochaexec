2e8aafc466eaf54d82bf804d9219dafc3bb9b400
Butterroach <butterroach@outlook.com>
2026-01-01 02:29:50 +0300

    update copyright statements

--- src/branding.rs
+++ src/branding.rs
@@ -1,3 +1,21 @@
+/*
+mochaexec: sudo if it was actually good. for responsible adults only.
+Copyright (C) 2025-2026 Butterroach
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // IMPORTANT!!! you MUST change these if you wanna fork mochaexec into an independent project!!!
 
 pub const PROJECT_NAME: &str = "mochaexec";
